4b140a2c2fb7ace8dcd804fe4a4a1a875916f00a
Ude Samson <udesamson89@gmail.com>
2026-06-20 08:56:26 +0100

    portfolio website

---
+++ js/script.js
@@ -0,0 +1,11 @@
+﻿const navToggle = document.querySelector('.nav-toggle');
+const siteNav = document.querySelector('.site-nav');
+const contactForm = document.querySelector('#contact-form');
+const formStatus = document.querySelector('#form-status');
+
+if (navToggle && siteNav) {
+  navToggle.addEventListener('click', () => {
+    siteNav.classList.toggle('open');
+  });
+}
+
