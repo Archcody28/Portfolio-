6d88bc812a387317968dd17e8ff1f9f350bff405
Ude Samson <udesamson89@gmail.com>
2026-06-21 21:28:51 +0100

    Update portfolio content and styling

--- js/script.js
+++ js/script.js
@@ -9,3 +9,28 @@ if (navToggle && siteNav) {
   });
 }
 
+const form = document.getElementById("contact-form");
+
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const name = document.getElementById("name").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const message = document.getElementById("message").value.trim();
+
+    const whatsappMessage =
+`Hello, my name is ${name}.
+
+Email: ${email}
+
+Message:
+${message}`;
+
+    const phoneNumber = "2349030868849"; 
+    const whatsappURL =
+        `https://wa.me/${phoneNumber}?text=${encodeURIComponent(whatsappMessage)}`;
+
+    window.open(whatsappURL, "_blank");
+
+    form.reset();
+});
